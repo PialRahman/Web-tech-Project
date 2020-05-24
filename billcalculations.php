8eaf79dd332e1b29814dce9aac6db26d1e7ae812
PialRahman <58075858+PialRahman@users.noreply.github.com>
2020-05-24 12:39:55 +0600

    bill calculation page

---
+++ billcalculations.php
@@ -0,0 +1,503 @@
+<!DOCTYPE html>
+<html>
+ <style type="text/css" media="screen">
+                #sort2{
+                  float:left;
+                }
+                 #sort3{
+                  float:left;
+                }
+                 #sort4{
+                  float:left;
+                }
+            
+         #sort5{
+                  float:left;
+                }
+                 #sort6{
+                  float:left;
+                }
+                 #sort7{
+                  float:left;
+                }
+                 #sort8{
+                  float:left;
+                }
+                 #sort9{
+                  float:left;
+                }
+                 #sort10{
+                  float:left;
+                }
+                 #sort11{
+                  float:left;
+                }
+                 #sort12{
+                  float:left;
+                }
+                 #sort13{
+                  float:left;
+                }
+                 #sort14{
+                  float:left;
+                }
+                 #sort15{
+                  float:left;
+                }
+                 #sort16{
+                  float:left;
+                }
+                 #sort17{
+                  float:left;
+                }
+                 #sort18{
+                  float:left;
+                }
+                 #sort19{
+                  float:left;
+                }
+                 #sort20{
+                  float:left;
+                }
+                 #sort21{
+                  float:left;
+                }
+                 #sort22{
+                  float:left;
+                }
+                 #sort23{
+                  float:left;
+                }
+                 #sort24{
+                  float:left;
+                }
+                 #sort25{
+                  float:left;
+                }
+                 #sort26{
+                  float:left;
+                }
+                 #sort27{
+                  float:left;
+                }
+                 #sort28{
+                  float:left;
+                }
+                #sort59{
+                  float: right;
+                 margin-top:-870px;
+                 margin-right:250px;
+                }
+
+           </style>
+    <head>
+      <style>
+       html{
+      background:url('categorybackground.jpg') no-repeat center center fixed;
+      - webkit -background-size:cover;
+      -o-background-size:cover;
+      background-size:cover;
+    }
+  </style>
+        
+        <title>Bill Calculation Page</title>
+        <meta charset="windows-1252">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <script>
+            
+            function calc()
+            {
+                var n1 = parseFloat(document.getElementById('n1').value);
+                
+                var oper = document.getElementById('item').value;
+                
+                if(oper === 'Chocolate Cold Coffee')
+                {
+                    document.getElementById('result').value = n1*500;
+                }
+                
+                if(oper === 'Caaramel Cold Coffee')
+                {
+                    document.getElementById('result').value = n1*650;
+                }
+                
+                if(oper === 'Vanilla Cold Coffee')
+                {
+                    document.getElementById('result').value = n1*800;
+                }
+                
+               
+            }
+             function calc2()
+            {
+                var n2 = parseFloat(document.getElementById('n2').value);
+                
+                var oper2 = document.getElementById('item2').value;
+                
+                if(oper2 === 'Chocolate Cold Coffee2')
+                {
+                    document.getElementById('result2').value = n2*500;
+                }
+                
+                if(oper2 === 'Caaramel Cold Coffee2')
+                {
+                    document.getElementById('result2').value = n2*650;
+                }
+                
+                if(oper2 === 'Vanilla Cold Coffee2')
+                {
+                    document.getElementById('result2').value = n2*800;
+                }
+                
+               
+            }
+             function calc3()
+            {
+                var n3 = parseFloat(document.getElementById('n3').value);
+                
+                var oper3 = document.getElementById('item3').value;
+                
+                if(oper3 === 'Chocolate Cold Coffee3')
+                {
+                    document.getElementById('result3').value = n3*500;
+                }
+                
+                if(oper3 === 'Caaramel Cold Coffee3')
+                {
+                    document.getElementById('result3').value = n3*650;
+                }
+                
+                if(oper3 === 'Vanilla Cold Coffee3')
+                {
+                    document.getElementById('result3').value = n3*800;
+                }
+                
+               
+            }
+              function calc4()
+            {
+                var n4 = parseFloat(document.getElementById('n4').value);
+                
+                var oper4 = document.getElementById('item4').value;
+                
+                if(oper4 === 'Latte Coffee')
+                {
+                    document.getElementById('result4').value = n4*1000;
+                }
+                
+                if(oper4 === 'Spanish Latte Coffee')
+                {
+                    document.getElementById('result4').value = n4*1200;
+                }
+                
+                if(oper4 === 'Espresso Coffee')
+                {
+                    document.getElementById('result4').value = n4*850;
+                }
+                
+               
+            }
+              function calc5()
+            {
+                var n5 = parseFloat(document.getElementById('n5').value);
+                
+                var oper5 = document.getElementById('item5').value;
+                
+                if(oper5 === 'Latte Coffee2')
+                {
+                    document.getElementById('result5').value = n5*1000;
+                }
+                
+                if(oper5 === 'Spanish Latte Coffee2')
+                {
+                    document.getElementById('result5').value = n5*1200;
+                }
+                
+                if(oper5 === 'Espresso Coffee2')
+                {
+                    document.getElementById('result5').value = n5*850;
+                }
+                
+               
+            }
+              function calc6()
+            {
+                var n6 = parseFloat(document.getElementById('n6').value);
+                
+                var oper6 = document.getElementById('item6').value;
+                
+                if(oper6 === 'Latte Coffee3')
+                {
+                    document.getElementById('result6').value = n6*1000;
+                }
+                
+                if(oper6 === 'Spanish Latte Coffee3')
+                {
+                    document.getElementById('result6').value = n6*1200;
+                }
+                
+                if(oper6 === 'Espresso Coffee3')
+                {
+                    document.getElementById('result6').value = n6*850;
+                }
+                
+               
+            }
+              function calc7()
+            {
+                var n7 = parseFloat(document.getElementById('n7').value);
+                
+                var oper7 = document.getElementById('item7').value;
+                
+                if(oper7 === 'Black Coffee')
+                {
+                    document.getElementById('result7').value = n7*1700;
+                }
+                
+                if(oper7 === 'Long Black Coffee')
+                {
+                    document.getElementById('result7').value = n7*2500;
+                }
+                
+                if(oper7 === 'Turkish Coffee')
+                {
+                    document.getElementById('result7').value = n7*2100;
+                }
+                
+               
+            }
+              function calc8()
+            {
+               
+                var n8 = parseFloat(document.getElementById('n8').value);
+                
+                var oper8 = document.getElementById('item8').value;
+                
+                if(oper8 === 'Black Coffee2')
+                {
+                    document.getElementById('result8').value = n8*1700;
+                }
+                
+                if(oper8 === 'Long Black Coffee2')
+                {
+                    document.getElementById('result8').value = n8*2500;
+                }
+                
+                if(oper8 === 'Turkish Coffee2')
+                {
+                    document.getElementById('result8').value = n8*2100;
+                }
+               
+            }
+              function calc9()
+            {
+                
+                var n9 = parseFloat(document.getElementById('n9').value);
+                
+                var oper9 = document.getElementById('item9').value;
+                
+                if(oper9 === 'Black Coffee3')
+                {
+                    document.getElementById('result9').value = n9*1700;
+                }
+                
+                if(oper9 === 'Long Black Coffee3')
+                {
+                    document.getElementById('result9').value = n9*2500;
+                }
+                
+                if(oper9 === 'Turkish Coffee3')
+                {
+                    document.getElementById('result9').value = n9*2100;
+                }
+               
+            }
+        </script>
+        
+    </head>
+    <body>
+     <h2>Select Item1:</h2>
+        <div id="sort2">
+         <select id="item">
+            <option value="Chocolate Cold Coffee">Chocolate Cold Coffee</option>
+            <option value="Caaramel Cold Coffee">Caaramel Cold Coffee</option>
+            <option value="Vanilla Cold Coffee">Vanilla Cold Coffee</option>
+        </select><br><br>
+      </div>
+      <div id="sort3">
+        <label for="n1">Amount:</label>
+        <input type="text" id="n1"/><br/><br/>
+      </div>
+        
+        <div id="sort4">
+         <label for="n1">Bill:</label>
+         <button onclick="calc();">=</button>
+        <input type="text" id="result"/><br><br>
+      </div><br><br>
+         <h2>Select Item2:</h2>
+         <div id="sort5">
+          <select id="item2">
+            <option value="Chocolate Cold Coffee2">Chocolate Cold Coffee</option>
+            <option value="Caaramel Cold Coffee2">Caaramel Cold Coffee</option>
+            <option value="Vanilla Cold Coffee2">Vanilla Cold Coffee</option>
+        </select><br><br>
+      </div>
+      <div id="sort6">
+        <label for="n2">Amount:</label>
+        <input type="text" id="n2"/><br/><br/>
+      </div>
+      <div id="sort7">
+         <label for="n2">Bill:</label>
+         <button onclick="calc2();">=</button>
+        <input type="text" id="result2"/></div><br><br>
+      
+          <h2>Select Item3:</h2> 
+          <div id="sort8">  
+         <select id="item3">
+            <option value="Chocolate Cold Coffee3">Chocolate Cold Coffee</option>
+            <option value="Caaramel Cold Coffee3">Caaramel Cold Coffee</option>
+            <option value="Vanilla Cold Coffee3">Vanilla Cold Coffee</option>
+        </select><br><br>
+      </div>
+      <div id="sort9">
+        <label for="n3">Amount:</label>
+        <input type="text" id="n3"/><br/><br/>
+      </div>
+      <div id="sort10">
+         <label for="n3">Bill:</label>
+         <button onclick="calc3();">=</button>
+        <input type="text" id="result3"/>
+      </div><br><br>
+           <h2>Select Item4:</h2>
+           <div id="sort11">
+         <select id="item4">
+            <option value="Latte Coffee">Latte Coffee</option>
+            <option value="Spanish Latte Coffee">Spanish Latte Coffee</option>
+            <option value="Espresso Coffee">Espresso Coffee</option>
+        </select><br><br>
+      </div>
+      <div id="sort12">
+        <label for="n4">Amount:</label>
+        <input type="text" id="n4"/><br/><br/>
+        </div>
+        <div id="sort13">
+         <label for="n4">Bill:</label>
+         <button onclick="calc4();">=</button>
+        <input type="text" id="result4"/></div><br><br>
+         <h2>Select Item5:</h2>
+         <div id="sort14">
+         <select id="item5">
+            <option value="Latte Coffee2">Latte Coffee</option>
+            <option value="Spanish Latte Coffee2">Spanish Latte Coffee</option>
+            <option value="Espresso Coffee2">Espresso Coffee</option>
+        </select><br><br>
+      </div>
+      <div id="sort15">
+        <label for="n5">Amount:</label>
+        <input type="text" id="n5"/><br/><br/>
+      </div>
+      <div id="sort16">
+         <label for="n5">Bill:</label>
+         <button onclick="calc5();">=</button>
+        <input type="text" id="result5"/></div><br><br>
+          <h2>Select Item6:</h2>
+          <div id="sort17">
+         <select id="item6">
+             <option value="Latte Coffee3">Latte Coffee</option>
+            <option value="Spanish Latte Coffee3">Spanish Latte Coffee</option>
+            <option value="Espresso Coffee3">Espresso Coffee</option>
+        </select><br><br>
+      </div>
+      <div id="sort18">
+        <label for="n6">Amount:</label>
+        <input type="text" id="n6"/><br/><br/>
+      </div>
+      <div id="sort19">
+         <label for="n6">Bill:</label>
+         <button onclick="calc6();">=</button>
+        <input type="text" id="result6"/></div><br><br>
+      <h2>Select Item7:</h2>
+      <div id="sort20">
+         <select id="item7">
+            <option value="Black Coffee">Black Coffee</option>
+            <option value="Long Black Coffee">Long Black Coffee</option>
+            <option value="Turkish Coffee">Turkish Coffee</option>
+        </select><br><br>
+      </div>
+      <div id="sort21">
+        <label for="n7">Amount:</label>
+        <input type="text" id="n7"/><br/><br/>
+      </div>
+       <div id="sort22">
+         <label for="n7">Bill:</label>
+         <button onclick="calc7();">=</button>
+        <input type="text" id="result7"/></div><br><br>
+          <h2>Select Item8:</h2>
+          <div id="sort23">
+         <select id="item8">
+            <option value="Black Coffee2">Black Coffee</option>
+            <option value="Long Black Coffee2">Long Black Coffee</option>
+            <option value="Turkish Coffee2">Turkish Coffee</option>
+        </select><br><br>
+      </div>
+      <div id="sort24">
+        <label for="n8">Amount:</label>
+        <input type="text" id="n8"/><br/><br/>
+      </div>
+      <div id="sort25">
+         <label for="n8">Bill:</label>
+         <button onclick="calc8();">=</button>
+        <input type="text" id="result8"/></div><br><br>
+         <h2>Select Item9:</h2>
+         <div id="sort26">
+         <select id="item9">
+            <option value="Black Coffee3">Black Coffee</option>
+            <option value="Long Black Coffee3">Long Black Coffee</option>
+            <option value="Turkish Coffee3">Turkish Coffee</option>
+        </select><br><br>
+      </div>
+      <div id="sort27">
+        <label for="n9">Amount:</label>
+        <input type="text" id="n9"/><br/><br/>
+        </div>
+        <div id="sort28">
+         <label for="n9">Bill:</label>
+         <button onclick="calc9();">=</button>
+        <input type="text" id="result9"/></div><br><br>
+  <script>
+function add(){
+var a,b,c,d,e,f,g,h,i;
+a=Number(document.getElementById("first").value);
+b=Number(document.getElementById("second").value);
+c=Number(document.getElementById("third").value);
+d=Number(document.getElementById("fourth").value);
+e=Number(document.getElementById("fifth").value);
+f=Number(document.getElementById("sixth").value);
+g=Number(document.getElementById("seventh").value);
+h=Number(document.getElementById("eighth").value);
+i=Number(document.getElementById("ninth").value);
+j= a + b + c + d + e + f + g + h + i;
+document.getElementById("answer").value= j;
+}
+</script>
+</head>
+  <div id="sort59">
+Item1 : <input id="first"><br><br>
+Item2: <input id="second"><br><br>
+Item3: <input id="third"><br><br>
+Item4 : <input id="fourth"><br><br>
+Item5 : <input id="fifth"><br><br>
+Item6 : <input id="sixth"><br><br>
+Item7 : <input id="seventh"><br><br>
+Item8 : <input id="eighth"><br><br>
+Item9 : <input id="ninth"><br><br>
+<button onclick="add()">Total Bills</button>
+<input id="answer"><br><br>
+Now <a href="billprints.php">Print</a><br><br>
+<button type="button" onclick="document.getElementById('demo').innerHTML = 'Order has been delivered!'">Deliver The Order</button>
+<p id="demo">Make The Delivery</p>
+</div>
+</form>
+    </body>
+</html>
