a5ab99b327937b8cdc451a90522973221a012f46
PialRahman <58075858+PialRahman@users.noreply.github.com>
2020-05-24 12:24:08 +0600

    Update category.php

--- category.php
+++ category.php
@@ -2,12 +2,21 @@
 <html>
 <head>
 	<title>Catergory</title>
+<style>
+    html{
+      background:url('categorybgimage3.jpg') no-repeat center center fixed;
+      - webkit -background-size:cover;
+      -o-background-size:cover;
+      background-size:cover;
+    }
+  </style>
 </head>
-<body  bgcolor="#FFCC99">
-<center><h1>Choose Your Category</h1></center>
+<body>
+<center><h1>Log In As</h1></center>
+<br>
+<a href="Firstpage.html" target=""><h2>Back</h2></a>
 <a href="adminlogin.php" target=""><center><img src="adminicon.png" alt="admin icon" width="140" height="120"></center></a>
-<center><a href="adminlogin.php" target="">Admin</a></center>
-
+<center><a href="adminlogin.php" target="">Admin</a><h3>(For Administrators Only)</h3></center>
 <a href="customerlogin.php" target=""><center><img src="customericon.png" alt="customericon icon" width="140" height="120"></center></a>
 <center><center><a href="customerlogin.php" target="">Customer</a></center>
 <a href="employeelogin.php" target=""><center><img src="employee.png" alt="employee icon" width="140" height="120"></center></a>
