f618170420c403de0c20636e2a4e60beacfe0b9c
PialRahman <58075858+PialRahman@users.noreply.github.com>
2020-05-24 12:46:31 +0600

    coffee menu page

---
+++ coffeemenu.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Coffee Menu</title>
+	<title>Customer Log In</title>
+   <style>
+    html{
+      background:url('coffeeitemspage.jpg') no-repeat center center fixed;
+      - webkit -background-size:cover;
+      -o-background-size:cover;
+      background-size:cover;
+    }
+    #setups11{
+      float:left;
+      margin-right:50px;
+    }
+     #setups12{
+      float:left;
+      margin-right:50px;
+   
+    }
+     #setups13{
+      float:left;
+      margin-right:50px;
+    }
+   #cleardvs{
+    clear:both ;
+   }
+    #setups21{
+      float:left;
+      margin-right:50px;
+    }
+     #setups22{
+      float:left;
+      margin-right:50px;
+   
+    }
+     #setups23{
+      float:left;
+      margin-right:50px;
+    }
+   #cleardvs2{
+    clear:both ;
+   }
+    #setups31{
+      float:left;
+      margin-right:50px;
+    }
+     #setups32{
+      float:left;
+      margin-right:50px;
+   
+    }
+     #setups33{
+      float:left;
+      margin-right:50px;
+    }
+ 
+  </style>
+</head>
+<body>
+<center>
+  <h1>Coffee Menu</h1>
+<h2>Iced Coffee</h2>
+</center>
+<div id="setups11">
+<img src="chocolatecoldcoffee.jpg" alt="Chocolate Cold Cofee" height="245" width="245"><p>Chocolate cold coffee</p><p>500tk</p>
+</div>
+<div id="setups12">
+<img src="caramelcoldcoffee.jpg" alt="Caramel Cold Cofee" height="245" width="245"><p>Caramel cold coffee</p><p>650tk</p>
+</div>
+<div id="setups13">
+<img src="vanillacoldcoffee.jpg" alt="Vanilla Cold Cofee" height="245" width="245"><p>Vanilla cold coffee</p><p>800tk</p>
+</div>
+<div id="cleardvs"></div>
+<br>
+<center>
+<h2>Hot Coffee</h2>
+</center>
+<div id="setups21">
+<img src="lattecoffee.jpg" alt="Latte Cofee" height="245" width="245"><p>Latte coffee</p><p>1000tk</p>
+</div>
+<div id="setups22">
+<img src="spanishlattecoffee.jpg" alt="Spanish Latte Cofee" height="245" width="245"><p>Spanish Latte coffee</p><p>1200tk</p>
+</div>
+<div id="setups23">
+<img src="espresso.jpg" alt="Espresso Cofee" height="245" width="245"><p>Espresso coffee</p><p>850tk</p>
+</div>
+<div id="cleardvs2"></div>
+<center>
+<h2>Black Coffee</h2>
+</center>
+<div id="setups31">
+<img src="blackcoffee.jpg" alt="Black Cofee" height="245" width="245"><p>Black coffee</p><p>1700tk</p>
+</div>
+
+<div id="setups32">
+<img src="longblackcoffee.jpg" alt="Long Black Cofee" height="245" width="245"><p>Long Black coffee</p><p>2500tk</p>
+</div>
+
+<div id="setups33">
+<img src="turkishcoffee.jpg" alt="Turkish Coffee" height="245" width="245"><p>Turkish coffee</p><p>2100tk</p>
+</div>
+</body>
+</html>
