c904640bfa787163084ee4a9e023db7ed51cadfd
PialRahman <58075858+PialRahman@users.noreply.github.com>
2020-05-24 12:49:39 +0600

    employee profile

---
+++ employeeprofile.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Employee Page</title>
+</head>
+<style type="text/css">
+  #pageside5{
+    background-color:lightgreen;
+    float:left;
+    width:250px;
+    height:700px;
+  }
+  #pageside5 ul{}
+  #pageside5 ul li{
+    list-style-type:none;
+    margin-bottom:40px;
+      }
+  #pageside5 ul li a{
+    text-decoration:none;
+      color:white;
+      font-size:20px;
+  }
+  #pageside5 ul li a:hover{
+    background-color:green;
+    width:250px;
+    height:20px;
+
+  }
+  #informations5{
+    float:left;
+    margin-left:200px;
+  }
+  
+  </style>
+<body>   
+    <center>
+            <div style="background-color:lightgreen">
+           <h3>Hi There!</h3>
+          <p align=right>  <h3>Welcome To your Profile</h3>
+            
+        </div>
+         <div id="pageside5">
+          <ul>
+            <li><a href="Firstpage.html">Home</li>
+              <li><a href="coffeemenu.php">View Menu</li>
+                <li><a href="Recipepage.php">View Recepies</li>
+                <li><a href="vieworders.php">View Orders</li>
+                  <li><a href="billcalculations.php">Make Bill</li>
+                      <li><a href="employeelogin.php">Log Out</li>
+          </ul>
+        </div>
+
+<center><img src="employee.png" alt="Employee icon" width="240" height="200"></a><br></center>
+<div id="informations5">
+<h2>Name:raju</h2>
+<h2>Email:raju@gmail.com</h2>
+<h2>Phone:01765432123</h2>
+</center>
+
+</body>
+</html>
