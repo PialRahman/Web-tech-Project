33e4fddfbd099f81b7476aed4d149767e9807e3d
PialRahman <58075858+PialRahman@users.noreply.github.com>
2020-05-24 12:47:24 +0600

    customer page

---
+++ customerpage.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Customer Panel</title>
+</head>
+<style type="text/css">
+	#pageside{
+		background-color:brown;
+		float:left;
+		width:250px;
+		height:700px;
+	}
+	#pageside ul{}
+	#pageside ul li{
+		list-style-type:none;
+		margin-bottom:40px;
+			}
+	#pageside ul li a{
+		text-decoration:none;
+			color:white;
+			font-size:20px;
+	}
+	#pageside ul li a:hover{
+		background-color:#802A00;
+		width:250px;
+		height:20px;
+
+	}
+	#informations{
+		float:left;
+		margin-left:200px;
+	}
+	
+	</style>
+<body>
+<div style="background-color:brown">
+<center><h1>Hi There</h1></center>cn
+<center><h1>Welcome To your Profile</h1></center>
+</div>
+
+
+        <div id="pageside">
+          <ul>
+            <li><a href="Firstpage.html">Home</li>
+              <li><a href="coffeemenu.php">View Menu</li>
+                <li><a href="Orderpage2.php">Order</li>
+                  <li><a href="https://accounts.google.com/">Send Feedback and Report</li>
+                      <li><a href="customerlogin.php">Log Out</li>
+          </ul>
+        </div>
+
+<center><img src="displaylogin.png" alt="login icon" width="240" height="200"></a><br></center>
+<div id="informations">
+<h2>Name:shofik</h2>
+<h2>Email:shofik@gmail.com</h2>
+<h2>Phone:01876543212</h2>
+</div>
+</body>
+</html>
