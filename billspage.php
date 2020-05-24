5d903f4661cd5a6f5ea2560a95c45843bb8aeaca
PialRahman <58075858+PialRahman@users.noreply.github.com>
2020-05-24 12:42:27 +0600

    Add files via upload

---
+++ billspage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bill Page</title>
+</head>
+<body>
+	<center>
+<h1>Thanks For Using Our App!!</h1>
+<h3>Your Bill is:<?php echo $_POST["bil"]?> tk </h3>
+</center>
+</body>
+</html>
