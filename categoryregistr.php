fe029bee2c88f24db9c6a6a09a60aff2274c972b
PialRahman <58075858+PialRahman@users.noreply.github.com>
2020-05-24 12:44:32 +0600

    register page

---
+++ categoryregistr.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Catergory</title>
+<style>
+    html{
+      background:url('categorybgimage3.jpg') no-repeat center center fixed;
+      - webkit -background-size:cover;
+      -o-background-size:cover;
+      background-size:cover;
+    }
+  </style>
+</head>
+<body>
+<center><h1>Register As</h1></center>
+<br>
+<a href="Firstpage.html" target=""><h2>Back</h2></a>
+
+<center>
+<a href="registercustomer.php" target=""><center><img src="customericon.png" alt="customericon icon" width="140" height="120"></center></a>
+<center><center><a href="registercustomer.php" target="">Customer</a></center>
+<a href="registeremployee.php" target=""><center><img src="employee.png" alt="employee icon" width="140" height="120"></center></a>
+<center><a href="registeremployee.php" target="">Employee</a></center>
+</body>
+</html>
