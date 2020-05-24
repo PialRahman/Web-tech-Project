58debab0d7aee720a73c0641aa827b898d5eead9
PialRahman <58075858+PialRahman@users.noreply.github.com>
2020-05-24 12:58:00 +0600

    reset password page

---
+++ Resetpasswordpage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Reset Password Page</title>
+	<link rel="stylesheet" type="text/css" href="loginStyle.css">
+</head>
+<style>
+    html{
+      background:url('categorybackground.jpg') no-repeat center center fixed;
+      - webkit -background-size:cover;
+      -o-background-size:cover;
+      background-size:cover;
+    }
+  </style>
+<body>
+	<center><h1>Reset Password</h1></center>
+	<center>
+		<br><br>
+		<h5>Enter Your Email address and press the 'Send Verfication Code' button.A verification link will be sent to your email.</h5>
+		Enter Email:<input type="text" name="eml" value=""><br><br>
+		<input type="submit" value="Send Verification Code">
+	</center>
+</body>
+</html>
