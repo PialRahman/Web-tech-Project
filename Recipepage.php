e63b9841a780ca9ddcac2924fde88d248496536a
PialRahman <58075858+PialRahman@users.noreply.github.com>
2020-05-24 12:54:24 +0600

    reciepe page

---
+++ Recipepage.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Recipe Page</title>
+</head>
+<style type="text/css">
+	#forms{
+	
+		font-size:80px;
+		color:#B31E00;
+		font-family:Cooper Std;
+	}
+	#bulletpoint{
+		font-size:20px;
+		font-family:impact;
+	}
+	#prepare{
+		border:black solid 3px;
+	}
+</style>
+	
+</style>
+<body bgcolor=#ffb319>
+<div id="forms">
+	<center>Recipes</center>
+</div>
+<div id="bulletpoint">
+	<h1>Iced Coffee</h1>
+</div>
+	<div id="prepare">
+	<h2>1.Chocolate Cold Coffee Recipe</h2>
+	<p>Ingredients
+		<ul>
+<li>200 ml chilled milk</li>
+<li>2 tbsp fresh cream</li>
+<li>2 tbsp health drink (chocolate) powder</li>
+<li>1 tbsp nutella (optional)</li>
+<li>1/2 tsp instant coffee</li>
+<li>2 tsp sugar</li>
+<li>1 tsp chocolate sauce</li>
+<li>4-5 ice cubes</li>
+</ul>
+Drizzle the inside of a glass with chocolate sauce.<br>
+In a mixer jar, add the rest of the ingredients and<br>
+ give it a good whiz.Pour the prepared coffee into the glass.<br>
+ Sprinkle some chocolate powder over the top.Serve immediately. Enjoy!</p>
+</div>
+<br>
+<div id="prepare">
+	<h2>2.Caramel Cold Coffee Recipe</h2>
+	<p>INGREDIENTS
+		<ul>
+<li>1 1/2 cups strong hot brewed coffee</li>
+<li>1/4 cup caramel sauce, plus a little extra for garnish</li>
+<li>2-3 tablespoons sugar, to taste</li>
+<li>coffee ice cubes or regular will work too</li>
+<li>1 1/2 cups cold milk or half-and-half</li>
+<li>whipped cream, for garnish (optional)</li>
+<li>ground cinnamon, for garnish (optional)</li>
+</ul>
+
+After the coffee is finished brewing and is hot and fresh, pour 1 1/2 cups into a measuring cup. Stir in the caramel sauce. If you add the caramel sauce straight into the measuring cup, and you are using a 2 cup measuring cup, pour in the Caramel Syrup until it reached 1 3/4 cup with the coffee. Stir well.
+Add your sugar and stir again until all sugar and caramel sauce is dissolved completely.
+Place in the refrigerator and chill until cold.
+When it's time to serve, divide your coffee mixture between two chilled glasses filled with ice (hint: coffee ice cubes are excellent here)
+Add your milk or half-and-half to top the glass.
+If you want the fancy coffee shop look, garnish with whipped cream and ground cinnamon if desired. Enjoy!
+	</p>
+	</div>
+	<br>
+	<div id="prepare">
+	<h2>3.Vanilla Cold Coffee Recipe</h2>
+	<p>Ingredients
+		<ul>
+<li>2 cups strongly brewed coffee, cold</li>
+<li>1/2 cup milk or cream</li>
+<li>2 tablespoons vanilla syrup</li>
+<li>1 cup ice</li>
+Fill two glasses with ice.  Mix together coffee, milk/cream, and vanilla syrup before pouring over ice.  Serve immediately.
+	</p>
+</div>
+	<div id="bulletpoint">
+		<h1>Hot Coffee</h1>
+	</div>
+	<div id="prepare">
+		<h2>1.Latte Coffee Recipe</h2>
+		<p>Ingredients
+			<ul>
+			<li>2 cups milk</li>
+			<li>1/3 cups hot freshly brewed dark roast espresso coffee</li>
+		</ul>
+			Heat milk in a saucepan set over medium-low heat. Whisk briskly with a wire whisk to create foam. Brew espresso and pour into 4 cups. Pour in milk, holding back the foam with a spoon. Spoon foam over the top.
+		</p>
+	</div>
+
+	<br>
+	<div id="prepare">
+		<h2>2.Spanish Latte Coffee Recipe</h2>
+		<p>Ingredients
+			<ul>
+			<li>1 cup strong brewed coffee</li>
+<li>1 tablespoon dairy-free chocolate syrup</li>
+<li>1 teaspoon orange zest (can sub ½ teaspoon orange extract)</li>
+<li>¼ teaspoon ground cinnamon</li>
+<li>⅔ cup dairy-free chocolate milk beverage beverage (like Almond Breeze)</li>
+<li>Dairy-free whipped topping, optional</li>
+		</ul>
+In a large mug, whisk together the coffee, chocolate syrup, orange zest, and cinnamon.
+Heat and froth the milk beverage until hot and foamy.
+Pour the milk beverage over the coffee.
+Top with whipped topping, if desired.
+		</p>
+	</div><br>
+	<div id="prepare">
+		<h2>3.Espresso Coffee Recipe</h2>
+		<p>Ingredients
+			<ul>
+			<li>1/2 cup ground coffee (French or other dark roast)</li>
+             <li>1-1/2 cups cold water</li>
+<li>Lemon twists, optional</li>
+		</ul>
+Place ground coffee in the filter of a drip coffeemaker. Add water; brew according to manufacturer's instructions. Serve immediately in espresso cups with lemon twists if desired.
+		</p>
+	</div>
+<div id="bulletpoint">
+		<h1>Black Coffee</h1>
+	</div>
+	<div id="prepare">
+		<h2>1.Black Coffee Recipe</h2>
+		<p>Ingredients
+			<ul>
+			<li>Water - 1 cup</li>
+<li>Instant Coffee - ½ tsp</li>
+<li>Brown Sugar - 1 tsp</li>
+		</ul>
+Bring water to a boil.
+Take coffee and sugar in a cup, add little hot water and mix well.
+Now add more water and mix well.
+Serve hot.
+		</p>
+	</div><br>
+		<div id="prepare">
+		<h2>2.Long Black Coffee Recipe</h2>
+		<p>Ingredients
+			<ul>
+		<li>Single or double shot of espresso</li>
+<li>180ml-250ml of hot water out of the espresso machine to top it up.</li>
+		</ul>
+Make a shot of espresso.Boil filtered water in a kettle & pour into a mug about 2/3 of the way up.Pour the espresso shot into the mug, adding sugar to taste.
+		</p>
+	</div><br>
+	<div id="prepare">
+		<h2>3.Turkish Coffee Recipe</h2>
+		<p>Ingredients
+			<ul>
+			<li>water</li>
+    <li>ground coffee</li>
+<li>sugar, optional</li>
+		</ul>
+First, mix coffee and water. Using a teaspoon, mix your finely ground coffee with cold water in a Turkish Coffee pot, gently until well combined. The ratio for a desired consistency is 1 coffee cup cold water and 2 teaspoons ground coffee. If you want your coffee sweet, add sugar along with coffee and water. Never add it after your coffee is cooked.
+		</p>
+	</div>
+</body>
+</html>
